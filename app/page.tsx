3bc56363ff9fd4976d9759a58dadc14b7cf539e4
tauhid97k <tauhid.coder@gmail.com>
2025-04-16 18:35:11 +0600

    added links for all the pages in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="text-center mt-8 h-screen grid place-items-center">
+      <div className="text-center">
+        <h1>Home Page</h1>
+        <div className="flex items-center gap-4 mt-8">
+          <Button asChild>
+            <Link href="/dashboard">Dashboard</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/dashboard/first-card">First Card</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/dashboard/second-card">Second Card</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/dashboard/daily-summery">Daily Summery</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
